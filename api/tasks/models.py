5d4b37005563aeb457a8356ae4aa8cc72fab3610
Kekeletso Liphoko <75853126+DigitalPyxie@users.noreply.github.com>
2026-05-07 06:52:18 +0000

    created task model

--- api/tasks/models.py
+++ api/tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True) #add* sets time only once
+    updated_at = models.DateTimeField(auto_now=True) #continuously updates
+
+    def __str__(self):
+        return self.title
