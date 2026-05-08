c603588563dce19a68aa8674434fec1b9e48199d
Kekeletso Liphoko <75853126+DigitalPyxie@users.noreply.github.com>
2026-05-08 02:53:45 +0000

    created and ran unit test file

--- api/tasks/tests.py
+++ api/tasks/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import Task
 
-# Create your tests here.
+class TaskAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.task = Task.objects.create(
+            title="Test Task",
+            description="Test Description",
+            is_completed=False
+        )
+
+    #test GET all tasks
+    def test_view_tasks(self):
+        response = self.client.get("/api/tasks/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    #test GET one task   
+    def test_fetch_task(self):
+        response = self.client.get(f"/api/tasks/{self.task.id}") 
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    #test POST task
+    def test_create_task(self):
+        data = {
+            "title": "New Task",
+            "description": "Task Description",
+            "is_completed": False
+        }
+        response = self.client.post("/api/tasks/", data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    #test PUT a task
+    def test_update_task(self):
+        data = {
+            "title": "Updated Task",
+            "description": "Updated Description",
+            "is_completed": True
+        }
+        response = self.client.put(f"/api/tasks/{self.task.id}", data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    #test DELETE task
+    def  test_delete_task(self):
+        response = self.client.delete(f"/api/tasks/{self.task.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)   
