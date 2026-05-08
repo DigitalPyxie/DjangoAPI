c236da67ce50a54e3c876b4e00af3766e1cba8ec
Kekeletso Liphoko <75853126+DigitalPyxie@users.noreply.github.com>
2026-05-08 03:21:29 +0000

    included logging in views and settings files

--- api/api/settings.py
+++ api/api/settings.py
@@ -117,3 +117,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+#Logging configuration 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
--- api/tasks/views.py
+++ api/tasks/views.py
@@ -1,8 +1,13 @@
+import logging
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from .models import Task
 from .serializers import TaskSerializer
+
+#creating a logger
+logger = logging.getLogger(__name__)
 
 @api_view(['GET', 'POST'])
 def task_list_create(request):
@@ -16,9 +21,17 @@ def task_list_create(request):
         serializer = TaskSerializer(data=request.data)
 
         if serializer.is_valid():
-            serializer.save()
+            task = serializer.save()
+
+            #POST logger*
+            logger.info(f"Task created: {task.title}")
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
+        #ERROR logger *
+        logger.error(serializer.errors)
+
+        #validation failure
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -39,13 +52,26 @@ def task_detail(request, pk):
         serializer = TaskSerializer(task,data=request.data)
 
         if serializer.is_valid():
-            serializer.save()
+            task = serializer.save()
+
+            #PUT logger*
+            logger.info(f"Task updated: {task.title}")
+
             return Response(serializer.data)
 
+        #ERROR logger*
+        logger.error(serializer.errors)
+
+        #Validation failure    
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     #to delete
     if request.method == 'DELETE':
+
+        #DELETE logger**
+        logger.info(f"Task deleted: {task.title}")
+
         task.delete()
+
         return Response(status=status.HTTP_204_NO_CONTENT)         
 
